cd54e2364a50356f73e46f750794ee3e00f9bde4
unknown <hendisyaputra@student.uir.ac.id>
2025-03-16 13:57:30 +0700

    Tutorial VueJs

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import Test1 from './components/Test1.vue'
+// import Test2 from './components/Test2.vue'
+// import Test3 from './components/Test3.vue'
+import Test4 from './components/Test4.vue'
+import Test5 from './components/Test5.vue'  
+</script>
+
+<template>
+ <!-- <Test1 />
+<Test2 />
+<Test3 /> -->
+<Test4 />
+<Test5 />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test1.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({count: 0})
+const message = ref('Hari ini sangat cerah')
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p>Count is: {{ counter.count }}</p>
+  </div>
+</template>
